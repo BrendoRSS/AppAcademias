458eaea03988a5cfb09bf8b30a220f147257e4ca
BrendoSilva <brendoricardo00@gmail.com>
2024-11-21 17:25:43 -0300

    WIP: Salvando alterações antes da atualização

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyBYUio86iWBn7KIgt599hyWqrQWf8WgVi8",
-    authDomain: "bodyfitness-9a0b7.firebaseapp.com",
-    projectId: "bodyfitness-9a0b7",
-    storageBucket: "bodyfitness-9a0b7.firebasestorage.app",
-    messagingSenderId: "1043755943081",
-    appId: "1:1043755943081:web:86537a4e6ca347d81f55b8",
-    measurementId: "G-99N70RCZ1F"
+  firebaseConfig: {
+    apiKey: "preencha aqui",
+    authDomain: "preencha aqui",
+    projectId: "preencha aqui",
+    storageBucket: "preencha aqui",
+    messagingSenderId: "preencha aqui",
+    appId: "preencha aqui"
   }
   
 };
