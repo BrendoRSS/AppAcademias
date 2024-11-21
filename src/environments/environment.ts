5ae79e8c056b55d7d83232d363aa16738b372ea6
Hebertsfj <hebertsdefreitas@gmail.com>
2024-11-21 15:12:56 -0300

    conserto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBYUio86iWBn7KIgt599hyWqrQWf8WgVi8",
+    authDomain: "bodyfitness-9a0b7.firebaseapp.com",
+    projectId: "bodyfitness-9a0b7",
+    storageBucket: "bodyfitness-9a0b7.firebasestorage.app",
+    messagingSenderId: "1043755943081",
+    appId: "1:1043755943081:web:86537a4e6ca347d81f55b8",
+    measurementId: "G-99N70RCZ1F"
+  }
+  
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
